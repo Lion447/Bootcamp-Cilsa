31be1bd5fde301d818e46d6a65e907f737b0d2fd
Lion447 <113065883+Lion447@users.noreply.github.com>
2024-11-04 21:51:34 -0300

    update de script
    Elimine un comentario del codigo

--- Tarea Clase 5/script.js
+++ Tarea Clase 5/script.js
@@ -10,34 +10,6 @@ function validarEmail() {
     }
 }
 
-/*
-document.getElementById('cambiar').addEventListener('click', function() {
-    cambiarColor('#f0f0f0', 'black', '#fff', '#007BFF', '#ccc', 'white');
-});
-
-document.getElementById('altoContraste').addEventListener('click', function() {
-    cambiarColor('#000000', 'white', '#333333', '#FFD700', '#999999', 'black');
-});
-
-function cambiarColor(fondo, colorTexto, fondoContenedor, colorBoton, colorBorde, colorTextoBoton) {
-    document.body.style.backgroundColor = fondo;
-    document.body.style.color = colorTexto;
-    document.querySelector('.container').style.backgroundColor = fondoContenedor;
-
-    const inputs = document.querySelectorAll('input[type="text"], input[type="email"], input[type="date"], select');
-    inputs.forEach(input => {
-        input.style.backgroundColor = fondoContenedor;
-        input.style.color = colorTexto;
-        input.style.borderColor = colorBorde;
-    });
-
-    const botones = document.querySelectorAll('button');
-    botones.forEach(boton => {
-        boton.style.backgroundColor = colorBoton;
-        boton.style.color = colorTextoBoton;
-    });
-}
-*/
 function cambiarEstilo(tema) {
     var element = document.body;
     element.dataset.bsTheme = tema;
